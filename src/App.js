f9105d93f2f3da8621b1cfe93b26d341befaf217
Ernest <ernestduodu@hotmail.co.uk>
2021-09-14 14:43:55 +0100

    Added navigation between mulyiple pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router";
+import Header from "./components/Header";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header />
+      <main>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <p>
+            Products page
+        </p>
+    )
+}
+
+export default Products
